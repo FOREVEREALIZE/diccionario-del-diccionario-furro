093407cfdad242d51d0a0ca6bf059a9775ea6054
FOREVEREALIZE <felixcrat@gmail.com>
2023-09-11 19:43:07 +0200

    Fix Meaning order

--- src/components/Meaning.tsx
+++ src/components/Meaning.tsx
@@ -6,7 +6,7 @@ export default function Meaning({ number, type, inflection, definition }: {
 }) {
     return (
         <div className="flex flex-col gap-2">
-            <h1 className="text-black text-xl">{number}. {type}. {inflection}</h1>
+            <h1 className="text-black text-xl">{number}. "{inflection}" {type}.</h1>
             <h2 className="text-black text-xl px-5">{definition}</h2>
         </div>
     )
